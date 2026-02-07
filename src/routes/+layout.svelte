5381d6c8b21621ab98b2fccecd2768b05e139d51
yaokissi <41991890+yaokissi@users.noreply.github.com>
2026-02-07 23:58:12 +0100

    navbar #1 & img

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,11 +1,16 @@
-<script lang="ts">
-	import favicon from '$lib/assets/favicon.svg';
-
-	let { children } = $props();
-</script>
-
-<svelte:head>
-	<link rel="icon" href={favicon} />
-</svelte:head>
-
-{@render children?.()}
+                <nav>
+                    <div class="logo">
+                        <a href="">
+                            Cooki.co
+                        </a>
+                    </div>
+                    <div class="menu">
+                        <ul>
+                            <li><a href="">Franchise</a></li>
+                            <li><a href="">Catering</a></li>
+                            <li><a href="">Shipping</a></li>
+                            <li><a href="">Locations</a></li>
+                        </ul>
+                    </div>
+                    <a href="" class="cta-btn">Buy Now</a>
+                </nav>

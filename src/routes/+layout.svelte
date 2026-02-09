ae10e0cffa0a7c86367ed9940a6eec97b71647a0
yaokissi <41991890+yaokissi@users.noreply.github.com>
2026-02-10 00:28:12 +0100

    delete scss

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,16 +1,48 @@
-                <nav>
-                    <div class="logo">
-                        <a href="">
-                            Cooki.co
-                        </a>
-                    </div>
-                    <div class="menu">
-                        <ul>
-                            <li><a href="">Franchise</a></li>
-                            <li><a href="">Catering</a></li>
-                            <li><a href="">Shipping</a></li>
-                            <li><a href="">Locations</a></li>
-                        </ul>
-                    </div>
-                    <a href="" class="cta-btn">Buy Now</a>
-                </nav>
+<nav>
+    <div class="logo">
+        <a href="#"> Cooki.co </a>
+    </div>
+    <div class="menu">
+        <ul>
+            <li><a href="#">Franchise</a></li>
+            <li><a href="#">Catering</a></li>
+            <li><a href="#">Shipping</a></li>
+            <li><a href="#">Locations</a></li>
+        </ul>
+    </div>
+    <a href="#" class="cta-btn">Buy Now</a>
+</nav>
+
+<style>
+    nav{
+        padding: 1.5vh 0;
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+    }
+
+    nav .logo a{
+        text-decoration: none;
+        font-size: 2rem;
+        font-weight: 800;
+        color: #552c10;
+    }
+
+    nav .menu ul{
+        display: flex;
+    }
+
+    nav .menu ul li{
+        margin: 0 1vw;
+    }
+
+    nav .menu ul li a{
+        color: #000;
+        transition: 2s;
+    }
+
+    nav .menu ul li a:hover{
+        color: #552c10;
+        font-weight: 600;
+    }
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,12 @@
 <h1>Jersey Makers V2</h1>
+
+
+<style>
+    h1{
+        font-size: 3rem;
+        font-weight: 800;
+        color: #552c10;
+        text-align: center;
+        margin-top: 5vh;
+    }
+</style>
